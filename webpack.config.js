7e332b74146e1b22422a762e61acb52d4c507ce6
hsehszroc <thewebsolver@gmail.com>
2021-06-13 16:52:26 +0545

    reformatting.. that's all...

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,19 @@
-const defaultConfig = require("./node_modules/@wordpress/scripts/config/webpack.config");
+const defaultConfig = require('./node_modules/@wordpress/scripts/config/webpack.config');
 // Require path.
-const path = require( 'path' );
+const path = require('path');
 
 module.exports = {
-	...defaultConfig,
-	entry: {
-		index: './src/index.js',
-		slider: './src/slider.js',
-	},
-	output: {
-		filename: '[name].js',
-		path: path.resolve(__dirname, 'build'),
-	},
-	module: {
-		...defaultConfig.module,
-		rules: [
-			...defaultConfig.module.rules,
-		]
-	}
+  ...defaultConfig,
+  entry: {
+    index: './src/index.js',
+    slider: './src/slider.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    ...defaultConfig.module,
+    rules: [...defaultConfig.module.rules]
+  }
 };
